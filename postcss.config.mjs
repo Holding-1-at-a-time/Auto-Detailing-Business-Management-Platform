338b4a5efd5ad128e77354c0598d6a0174f3fa95
holding-1-at-a-time <rromerojr1@gmail.com>
2025-05-26 00:46:59 -0500

    Update dependencies and configuration for authentication setup

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,19 @@
+/**
+    * @description      : 
+    * @author           : rrome
+    * @group            : 
+    * @created          : 25/05/2025 - 18:21:42
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 25/05/2025
+    * - Author          : rrome
+    * - Modification    : 
+**/
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
   },
 };
 
